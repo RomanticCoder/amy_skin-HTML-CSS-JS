d5c0c19384aeb03138f48476a7ab36554fd98552
lee <letmalikdothat@gmail.com>
2022-03-16 01:36:29 -0400

    navigation bar fixed

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,6 +2,8 @@
 const mobileLogo = document.querySelector(".navbar__logo.mobile")
 const desktopLogo = document.querySelector(".navbar__logo.desktop")
 const navBtn = document.querySelector("#navBtn")
+const navbar = document.querySelector("#navbar")
+const closeBtn = document.querySelector(".navbar__closeBtn")
 
 window.addEventListener("resize", (event) => {
     if (window.innerWidth > 650) {
@@ -21,15 +23,24 @@ window.addEventListener("scroll", (event) => {
     }
     // y 100 over > navbar--mobile X , burger icon O
 
+    //mobile only
+
+    if (navbar.style.display == "flex")
+        return;
     const pageY = (window.pageYOffset)
     if (pageY > 100) {
         navBtn.style.display = "block"
     } else {
         navBtn.style.display = "none"
     }
-    //
+
 })
 
 navBtn.addEventListener("click", () => {
+    navbar.style.display = "flex"
+    navBtn.style.display = "none"
+})
 
+closeBtn.addEventListener("click", () => {
+    navbar.style.display = "none"
 })
